507a82d5c2f33e2185b6850245b33b3df639640f
dennisdeepstate <dennis@ruracio.com>
2024-07-03 22:46:52 +0300

    banner

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,11 @@
+<section class="relative h-[calc(100vh-12rem)] w-full">
+	<div class="absolute bottom-[50%] text-center w-full">
+		<h1 class="text-7xl">who are you gonna call</h1>
+		<h1 class="text-7xl">ask who?? ask damaris</h1>
+	</div>
+	<img
+		alt="ask damaris banner"
+		class="absolute bottom-0 h-[100%] left-[37%]"
+		src="/damaris_banner.png"
+	/>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 <script>
+	import Hero from '$lib/components/hero.svelte'
 	import VideoSlider from '$lib/components/video_slider.svelte'
 </script>
+
+<Hero></Hero>
